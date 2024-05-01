ff066be3c9676af37771a1a9b72d10a56768fb51
Toniirubilar <laboralagus@gmail.com>
2024-04-30 22:49:20 -0300

    cambios

--- borrador.js
+++ borrador.js
@@ -4,29 +4,29 @@ const app = express();
 
 const port = 3000;
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send("<h1> Hola mundo! </h1>");
 })
 
 const listUser = [
     {
-        name:"pedro",
-        lastname:"pepito",
+        name: "pedro",
+        lastname: "pepito",
         age: 21,
-        hincha:"River",
+        hincha: "River",
     },
-    
+
     {
-        name:"Marina",
-        lastname:"Martinez",
+        name: "Marina",
+        lastname: "Martinez",
         age: 24,
-        hincha:"Boca Juniors",
+        hincha: "Boca Juniors",
     }
 ]
 
-app.get('/users', (req, res) =>{
-    res.json ({
-        ok:true,
+app.get('/users', (req, res) => {
+    res.json({
+        ok: true,
         listUser
     })
 })
@@ -34,29 +34,29 @@ app.get('/users', (req, res) =>{
 
 app.get('/user/find', (req, res) => {
 
-    const {query} = req;
-     console.log (query);
+    const { query } = req;
+    console.log(query);
     //  const name = query.name;
     //  const lastname = query.lastname;
-     const {name, lastname} = query;
-    
+    const { name, lastname } = query;
+
 
     const result = listUser.find((user) => user.name === name && user.lastname === lastname);
 
-     res.status(200).json({
-        msg:"se esta consultando",
+    res.status(200).json({
+        msg: "se esta consultando",
         result
     })
 })
 
 //Crear nuevo usuario:
 app.post('users/create', (req, res) => {
-    
-    const {name, lastname, age, hincha} = req.body;
 
-    listUser = (... listUser, {name, lastname, age, hincha})
+    const { name, lastname, age, hincha } = req.body;
 
-    const id = listUser(listUser.length-1).id+1;
+    listUser = (...listUser, { name, lastname, age, hincha })
+
+    const id = listUser(listUser.length - 1).id + 1;
     // const id = listUser.splice(listUser.length-1,listUser.length)
     listUser.push(newUser);
 
@@ -68,12 +68,20 @@ app.post('users/create', (req, res) => {
     )
 })
 //Editar un usuario
-app.put("users/edit/:id", (req, res) => {
-    const idEditar = req.params.id;
-    const {id} = req.body.id;
+app.put("users/edit/", (req, res) => {
+    const id = req.query.id;
     const newData = req.body;
-    listUser.push(req.body);
-})
+
+    const posUser = listUser.findIndex((user) => user.id == id);
+
+    if (posUser < 0) res.status(404).json({
+        ok: false,
+            msg: `No existe el usuario con id: ${id}`
+        })
+
+        listUser [posUser] = {... listUser[posUser], ... newData}
+    }
+)
 
 //Maneras de agregar usuarios
 // listUser.push(newUser);
@@ -81,25 +89,25 @@ app.put("users/edit/:id", (req, res) => {
 // listUser.push(... listUser. req.body):
 
 app.get('/user/find/id', (req, res) => {
-    const {query} = req;
-    const {id} = query;
+    const { query } = req;
+    const { id } = query;
     const result = listUser.find((user) => parseInt(user.id) == parseInt(id));
 
     if (result)
         res.status(200).json({
-            ok:true,
+            ok: true,
             result
         })
     else
         res.status(404).json({
-            ok:false,
-            msg:"No se Encontro"
+            ok: false,
+            msg: "No se Encontro"
         })
 })
 
 
 //Escuchamos en el puerto 3000
-app.listen(3000, ()=> {
+app.listen(3000, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
 })
 
